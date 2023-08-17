cc7f9e433e17346f95e195dedcd2bccddb98554a
Eloísa <118397019+e-0a@users.noreply.github.com>
2023-08-17 00:02:50 -0300

    -

--- projeto_cocria/01v.java
+++ projeto_cocria/01v.java
@@ -19,16 +19,16 @@ void draw{
     for(int x = 0; x < tiles; x++){
     for(int y = 0; y < tiles; y++){
       
-      color c = img.get(int(x * tileSize), int(y * tileSize));
-      float b = map(brightness(c), 0, 255, 1, 0);
+    color c = img.get(int(x * tileSize), int(y * tileSize));
+    float b = map(brightness(c), 0, 255, 1, 0);
       
-      float z = map(b, 0, 1, -100, 100);
+    float z = map(b, 0, 1, -100, 100);
       
-      push();
-      translate(x * tileSize - width/2, y * tileSize - height/2, z);
-      rotateY(width/2);
-      sphere(tileSize * b * 0.8);
-      pop();
+    push();
+    translate(x * tileSize - width/2, y * tileSize - height/2, z);
+    rotateY(width/2);
+    sphere(tileSize * b * 0.8);
+    pop();
       
     }
   }
